201777e081820c880e412e37cdf398d7308a65c0
罗勇 <luoyongsir@gmail.com>
2019-06-22 15:41:47 +0800

    添加实体类注释生成

--- src/main/java/com/mybatis/generator/internal/DefCommentGenerator.java
+++ src/main/java/com/mybatis/generator/internal/DefCommentGenerator.java
@@ -3,23 +3,65 @@ package com.mybatis.generator.internal;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
 
+import java.util.Properties;
+
 /**
  * 自定义实体属性注释
+ *
  * @author luoyong
  */
 public class DefCommentGenerator extends DefaultCommentGenerator {
 
-	@Override
-	public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-		String remarks = introspectedColumn.getRemarks();
-		if (StringUtility.stringHasValue(remarks)) {
-			String[] remarkLines = remarks.split(System.getProperty("line.separator"));
-			for (String remarkLine : remarkLines) {
-				field.addJavaDocLine("/** " + remarkLine + " */");
-			}
-		}
-	}
+    private Properties properties;
+
+    public DefCommentGenerator() {
+        super();
+        properties = new Properties();
+    }
+
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.properties.putAll(properties);
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                field.addJavaDocLine("/** " + remarkLine + " */");
+            }
+        }
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addJavaDocLine("/**");
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * This class corresponds to the database table ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        topLevelClass.addJavaDocLine(sb.toString());
+
+        String remarks = introspectedTable.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            topLevelClass.addJavaDocLine(" * Database Table Remarks:");
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                topLevelClass.addJavaDocLine(" *   " + remarkLine);
+            }
+            topLevelClass.addJavaDocLine(" *");
+        }
+
+        String author = properties.getProperty("author");
+        topLevelClass.addJavaDocLine(" * @author " + author);
+        topLevelClass.addJavaDocLine(" * @date " + getDateString());
+
+        topLevelClass.addJavaDocLine(" */");
+    }
+
 }
